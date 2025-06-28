f924bbb158ac9ceb469d6c910300a8cb5c05ecc6
Kelvin Henrique Nascimento <contato.henriquue@gmail.com>
2025-06-28 02:47:17 +0000

    mas não deu

--- src/app/download/page.tsx
+++
@@ -1,4 +0,0 @@
-// This page is no longer used and can be deleted.
-export default function DownloadPage() {
-  return null;
-}
--- src/components/copy-button.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-
-// This component is no longer used and can be deleted.
-export function CopyButton() {
-  return null;
-}
